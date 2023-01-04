ad4ddada39d97fad8f2b28f8be99b6944da0dd05
FahmidaIslamShammi <71556293+FahmidaIslamShammi@users.noreply.github.com>
2023-01-05 00:01:12 +0600

    Add files via upload

---
+++ convert temperature from  Celsius to Fahrenheit.py
@@ -0,0 +1,6 @@
+# Take the input which is consider as temperature in python
+C= float(input("enter the temperature ="))
+
+#Celsius to Fahrenheit conversion formula
+F = C * 1.8 + 32
+print("Temperature in fahrenheit is=", F)
